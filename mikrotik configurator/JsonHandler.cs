ca4c277687c02b1699955fa22228160f22219cde
azimuth <r.vanmontfort@student.fontys.nl>
2018-06-01 12:39:44 +0200

    added json handler and json for testing

---
+++ mikrotik configurator/JsonHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace mikrotik_configurator
+{
+    class JsonHandler
+    {
+        public JsonHandler()
+        {
+        }
+
+        public JsonObject ReadJson(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+            String file = File.ReadAllText(path); 
+            JsonObject jsonFile = JsonConvert.DeserializeObject<JsonObject>(file); //parsing json to a object for easier acess
+            return jsonFile;
+        }
+
+        public bool WriteJson(JsonObject json, string path)
+        {
+            if (File.Exists(path))
+            {
+               DialogResult result = MessageBox.Show("are you sure you want to overwrite this file?", "file already exists",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1);
+                if (result == DialogResult.No)
+                {
+                    return false;
+                }
+            }
+            String file = JsonConvert.SerializeObject(json);
+            File.WriteAllText(path,file);
+            return true;
+        }
+    }
+}
---
+++ mikrotik configurator/JsonObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mikrotik_configurator
+{
+    class JsonObject
+    {
+        public wifiSettings wifi { get; set; }
+        public cableSettings cable { get; set; }
+        public portSettings[] portforwards { get; set; }
+    }
+
+    class wifiSettings
+    {
+        public string ssid { get; set; }
+        public string password { get; set; }
+        public string ip { get; set; }
+        public int subnetmask { get; set; }
+        public string dnsserver { get; set; }
+
+    }
+
+    class cableSettings
+    {
+        public string ip { get; set; }
+        public string subnetmask { get; set; }
+    }
+
+    class portSettings
+    {
+        public string fromIp { get; set; }
+        public string toIp { get; set; }
+        public int inPort { get; set; }
+        public int outPort { get; set; }
+
+    }
+}
+
